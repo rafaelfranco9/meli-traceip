c7d8496dbf455c28991121c82bd0e3a7f2902f82
Rafael Franco <francosprivato@gmail.com>
2022-02-11 12:55:32 -0300

    updating countryResponseDTO to implement ICountry interface

--- src/countries/dto/countryResponse.dto.ts
+++ src/countries/dto/countryResponse.dto.ts
@@ -1,27 +1,26 @@
-import { coordinate } from "../interfaces";
+import { ICurrencies } from 'src/currencies/interfaces';
+import { coordinate } from 'src/geolocation/interfaces';
+import { convertToCurrency, getUTCtime } from '../helpers/countries.helpers';
+import { ICountry, ITimezone } from '../interfaces';
 
-export class CountryResponseDto {
-  public nameInSpanish: string;
-  public name: string;
-  public ISOcode: string;
-  public currencies: Record<string, currencyDetails>;
-  public coordinates: coordinate;
-  public timezones: string[];
-  public languages: Record<string, string>;
+export class CountryResponseDto implements ICountry {
+  spanishName: string;
+  commonName: string;
+  ISOcode: string;
+  languages: Record<string, string>;
+  timezones: ITimezone[];
+  currencies: ICurrencies[];
+  coordinates: coordinate;
+
   constructor(body: any) {
-    this.nameInSpanish = body.translations.spa.common;
-    this.name = body.name.common;
+    this.spanishName = body.translations.spa.common;
+    this.commonName = body.name.common;
     this.ISOcode = body.cca2;
-    this.currencies = body.currencies;
+    this.currencies = convertToCurrency(body.currencies);
     this.coordinates = body.latlng;
     this.languages = body.languages;
-    this.timezones = body.timezones;
+    this.timezones = body.timezones.map((time: string) =>
+      getUTCtime(time),
+    ) as ITimezone[];
   }
 }
-
-interface currencyDetails {
-  name: string;
-  symbol: string;
-}
-
-
