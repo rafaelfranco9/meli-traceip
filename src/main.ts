62d4f535f2d475a0661301b9d19a640b86c6c316
Rafael Franco <francosprivato@gmail.com>
2022-02-11 17:00:22 -0300

    adding PORT to .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  const PORT = config.get<string>('PORT');
+  await app.listen(PORT || 3000, () =>
+    Logger.log(`Server listening in port ${PORT}`, 'App'),
+  );
 }
 bootstrap();
