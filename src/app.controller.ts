0b640dee4f25e10ff0a760fcac341c3c87ab0465
Rafael Franco <francosprivato@gmail.com>
2022-02-11 13:14:43 -0300

    adding functions to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,12 @@
-import {
-  CACHE_MANAGER,
-  Controller,
-  Get,
-  Inject,
-  Param,
-  CacheInterceptor,
-} from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject, Param } from '@nestjs/common';
 import { Cache } from 'cache-manager';
-import { firstValueFrom, lastValueFrom, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { lastValueFrom } from 'rxjs';
 import { COORDINATES } from './countries/constants';
-import { CalculateDistanceInKm } from './countries/helpers/countries.helpers';
 import { CountriesService } from './countries/countries.service';
-import { CountryResponseDto } from './countries/dto/countryResponse.dto';
 import { CurrenciesService } from './currencies/currencies.service';
 import { GeolocationService } from './geolocation/geolocation.service';
+import { ICountry } from './countries/interfaces';
+import { ClientResponseDto } from './common/dto/clientResponse.dto';
 @Controller()
 export class AppController {
   constructor(
@@ -27,27 +19,25 @@ export class AppController {
   @Get('ip/:ip')
   async getCountryInformation(@Param('ip') ip: string): Promise<any> {
     try {
-      const { countryCode } = await lastValueFrom(
-        this.geolocationService.getCountryByIp(ip),
-      );
-      const country = await lastValueFrom(
-        this.countriesService.getCountryInformation(countryCode),
-      );
-      const exchangeRate = await lastValueFrom(
-        this.currenciesService.getExchangeRateInUsd(
-          Object.keys(country.currencies)[0],
-        ),
-      );
-      const distance = CalculateDistanceInKm(
-        COORDINATES.ARGENTINA,
-        country.coordinates,
-      );
+      let country: ICountry = await this.cacheManager.get(ip);
+      if (!country) {
+        const { countryCode } = await lastValueFrom(
+          this.geolocationService.getCountryByIp(ip),
+        );
+        country = await lastValueFrom(
+          this.countriesService.getCountryInformation(countryCode),
+        );
+        this.cacheManager.set(ip, country);
+      }
 
-      return {
+      await this.currenciesService.setUsdRates(country.currencies);
+
+      const response = new ClientResponseDto(
+        ip,
         country,
-        exchangeRate,
-        distance,
-      };
+        COORDINATES.ARGENTINA,
+      );
+      return response;
     } catch (err) {
       console.log(err.status);
     }
