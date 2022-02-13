34ec1f6880f859db94cd0dd0fb54519730520e88
Rafael Franco <francosprivato@gmail.com>
2022-02-13 08:36:30 -0300

    implementing cache manager in countries service

--- src/countries/countries.module.ts
+++ src/countries/countries.module.ts
@@ -1,11 +1,11 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { CurrenciesModule } from 'src/currencies/currencies.module';
 import { CountriesService } from './countries.service';
 
 @Module({
-  imports:[HttpModule,CurrenciesModule],
+  imports: [HttpModule, CurrenciesModule, CacheModule.register({ ttl: 0 })],
   providers: [CountriesService],
-  exports:[CountriesService]
+  exports: [CountriesService],
 })
 export class CountriesModule {}
--- src/countries/countries.service.ts
+++ src/countries/countries.service.ts
@@ -1,8 +1,14 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { CurrenciesService } from 'src/currencies/currencies.service';
+import { Cache } from 'cache-manager';
+import { catchError, lastValueFrom, map, throwError } from 'rxjs';
 import { CountryResponseDto } from './dto/countryResponse.dto';
 
 @Injectable()
@@ -11,28 +17,42 @@ export class CountriesService {
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {
     this.API_DATA = this.configService.get<string>('API_COUNTRIES_CODE');
   }
 
-  getCountryInformation(countryCode: string): Observable<CountryResponseDto> {
-    return this.httpService.get<any>(`${this.API_DATA}/${countryCode}`).pipe(
-      map((response) => response.data[0]),
-      map((body) => {
-        return new CountryResponseDto(body);
-      }),
-      catchError((error) => {
-        return throwError(
-          () =>
-            new HttpException(
-              error.message || 'Country not found',
-              error.status || HttpStatus.NOT_FOUND,
-            ),
-        );
-      }),
+  async getCountryInformation(
+    countryCode: string,
+  ): Promise<CountryResponseDto | null> {
+    let countryData = await this.cacheManager.get<CountryResponseDto>(
+      countryCode,
     );
+    if (!countryData) {
+      countryData = await lastValueFrom(
+        this.httpService.get(`${this.API_DATA}/${countryCode}`).pipe(
+          map((response) => {
+            const { status, data } = response;
+            if (status == HttpStatus.OK && data.length > 0) {
+              const countryResponse = new CountryResponseDto(data[0]);
+              this.cacheManager.set(countryCode, countryResponse);
+              return countryResponse;
+            }
+            return null;
+          }),
+          catchError((error) => {
+            return throwError(
+              () =>
+                new HttpException(
+                  error.message || 'Error requesting data to api',
+                  error.status || HttpStatus.NOT_FOUND,
+                ),
+            );
+          }),
+        ),
+      );
+    }
+
+    return countryData;
   }
-
-  
-
 }
