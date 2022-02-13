f918003951d23fc4a0d1c27bd26b3cea6310d5cc
Rafael Franco <francosprivato@gmail.com>
2022-02-13 10:21:31 -0300

    updating currencies service to be less coupled

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -5,11 +5,13 @@ import {
   HttpStatus,
   Inject,
   Injectable,
+  Logger,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cache } from 'cache-manager';
 import { catchError, lastValueFrom, map, throwError } from 'rxjs';
 import { CurrencyCodes } from './enums';
+import { calculateExchangeRateInUsd } from './helpers';
 import { ICurrencies } from './interfaces';
 
 @Injectable()
@@ -17,6 +19,7 @@ export class CurrenciesService {
   private API_DATA;
   private ACCESS_KEY;
   private CACHE_RATES_KEY: string = 'RATES';
+  private logger = new Logger(CurrenciesService.name);
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
@@ -26,55 +29,56 @@ export class CurrenciesService {
     this.ACCESS_KEY = this.configService.get('FIXER_ACCESS_KEY');
   }
 
-  async setUsdRates(currencies: ICurrencies[]) {
-    let rates = await this.cacheManager.get(this.CACHE_RATES_KEY);
-    if (!rates) {
-      rates = await this.getLastestRates();
+  async getCurrenciesWithUsdRate(
+    currencies: ICurrencies[],
+  ): Promise<ICurrencies[]> {
+    const rates = await this.getExchangeRates();
+    if (rates) {
+      return currencies.map((currency) => {
+        if (currency.code == CurrencyCodes.USD) {
+          currency.usdRate = 1;
+        } else if (
+          rates[currency.code] != null &&
+          rates[CurrencyCodes.USD] != null
+        ) {
+          currency.usdRate = calculateExchangeRateInUsd(
+            rates[CurrencyCodes.USD],
+            rates[currency.code],
+          );
+        }
+        return currency;
+      });
     }
-    currencies.map((currency) => {
-      if (currency.code == CurrencyCodes.USD) {
-        currency.usdRate = 1;
-      } else if (
-        rates[currency.code] != null &&
-        rates[CurrencyCodes.USD] != null
-      ) {
-        currency.usdRate = this.calculateExchangeRateInUsd(
-          rates[CurrencyCodes.USD],
-          rates[currency.code],
-        );
-      }
-    });
+    return currencies;
   }
 
-  async getLastestRates(): Promise<Record<string, number>> {
-    return await lastValueFrom(
-      this.httpService
-        .get(`${this.API_DATA}/latest?access_key=${this.ACCESS_KEY}`)
-        .pipe(
-          map((response) => {
-            if (response.status == HttpStatus.OK) {
-              return response.data.rates;
-            } else {
-              throwError(() => new Error('Error requesting exchange data'));
-            }
-          }),
-          map((rates) => {
-            this.cacheManager.set(this.CACHE_RATES_KEY, rates);
-            return rates;
-          }),
-          catchError((err) => {
-            return throwError(
-              () => new HttpException(err.message, HttpStatus.NOT_FOUND),
-            );
-          }),
-        ),
+  async getExchangeRates(): Promise<Record<string, number> | null> {
+    let rates = await this.cacheManager.get<Record<string, number>>(
+      this.CACHE_RATES_KEY,
     );
-  }
-
-  calculateExchangeRateInUsd(usdRate: number, currencyRate: number) {
-    const usd = 1 / usdRate;
-    const currRate = usd * currencyRate;
-    const exchangeRate = 1 / currRate;
-    return +exchangeRate.toFixed(5);
+    if (!rates) {
+      rates = await lastValueFrom(
+        this.httpService
+          .get(`${this.API_DATA}/latest?access_key=${this.ACCESS_KEY}`)
+          .pipe(
+            map((response) => {
+              if (response.status == HttpStatus.OK) {
+                return response.data.rates;
+              } else {
+                throwError(() => new Error('Error requesting exchange data'));
+              }
+            }),
+            map((rates) => {
+              this.cacheManager.set(this.CACHE_RATES_KEY, rates);
+              return rates;
+            }),
+            catchError((err) => {
+              this.logger.error(err.message);
+              return null;
+            }),
+          ),
+      );
+    }
+    return rates;
   }
 }
