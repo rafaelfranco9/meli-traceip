c1c260f4312f5e573b7e4f73468597677bbdd2b8
Rafael Franco <francosprivato@gmail.com>
2022-02-13 08:02:15 -0300

    implementing cache in geolocation service

--- src/geolocation/dto/geolocationResponse.dto.ts
+++ src/geolocation/dto/geolocationResponse.dto.ts
@@ -1,6 +1,12 @@
 export class GeolocationResponseDto {
-    readonly countryCode:string;
-    readonly countryCode3:string;
-    readonly countryName:string;
-    readonly countryEmoji:string;
+  countryCode: string;
+  countryCode3: string;
+  countryName: string;
+  countryEmoji: string;
+  constructor(body: any) {
+    this.countryCode = body.countryCode;
+    this.countryCode3 = body.countryCode3;
+    this.countryName = body.countryName;
+    this.countryEmoji = body.countryEmoji;
+  }
 }
--- src/geolocation/geolocation.service.ts
+++ src/geolocation/geolocation.service.ts
@@ -1,8 +1,15 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { catchError, lastValueFrom, map, Observable, throwError } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { GeolocationResponseDto } from './dto/geolocationResponse.dto';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class GeolocationService {
@@ -10,24 +17,38 @@ export class GeolocationService {
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {
     this.API_DATA = this.configService.get<string>('API_IP2COUNTRY');
   }
 
-  getCountryByIp(ip: string): Observable<GeolocationResponseDto> {
-    return this.httpService.get(`${this.API_DATA}/ip?${ip}`).pipe(
-      map((response) => {
-        return response.data;
-      }),
-      catchError((error) => {
-        return throwError(
-          () =>
-            new HttpException(
-              error.message || 'Invalid IP address',
-              error.response.status || HttpStatus.NOT_FOUND,
-            ),
-        );
-      }),
-    );
+  async getCountryCodeByIp(ip: string): Promise<GeolocationResponseDto | null> {
+    let code = await this.cacheManager.get<GeolocationResponseDto>(ip);
+    if (!code) {
+      code = await lastValueFrom(
+        this.httpService
+          .get<GeolocationResponseDto | null>(`${this.API_DATA}/ip?${ip}`)
+          .pipe(
+            map((response) => {
+              const { status, data } = response;
+              if (status == HttpStatus.OK) {
+                this.cacheManager.set(ip, code);
+                return new GeolocationResponseDto(data);
+              }
+              return null;
+            }),
+            catchError((error) => {
+              return throwError(
+                () =>
+                  new HttpException(
+                    error.message || 'Error requesting data to api',
+                    error.response.status || HttpStatus.NOT_FOUND,
+                  ),
+              );
+            }),
+          ),
+      );
+    }
+    return code;
   }
 }
